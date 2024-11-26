119ad8be4eb5e92558be1da56baef6387505a5be
zeko1244 <aalsdr61@gmail.com>
2024-11-26 03:03:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 
-# تحديث الحزم
-apt update && apt upgrade -y
-
-# تثبيت المتطلبات الأساسية
+# تحديث Termux وتثبيت الحزم الأساسية
+pkg update && pkg upgrade -y
 pkg install python git -y
 
-# استنساخ المشروع من GitHub
-echo "⏳ جاري تنزيل المشروع من GitHub..."
-git clone https://github.com/zeko1244/test ~/test
+# استنساخ المستودع
+git clone https://github.com/zeko1244/test.git
+cd test
 
-# الانتقال إلى مجلد المشروع
-cd ~/test
+# تثبيت المكتبات المطلوبة
+pip install -r requirements.txt
 
-# تثبيت المكتبات المطلوبة إذا كان هناك ملف requirements.txt
-if [ -f "requirements.txt" ]; then
-  echo "⏳ جاري تثبيت المكتبات المطلوبة..."
-  pip install -r requirements.txt
-else
-  echo "📋 لا يوجد ملف requirements.txt لتثبيت المكتبات."
-fi
-
-# تشغيل البوت
-if [ -f "snd.py" ]; then
-  echo "✅ تم تثبيت البوت بنجاح!"
-  python3 snd.py
-else
-  echo "❌ لم يتم العثور على الملف snd.py."
-fi
+# تشغيل البرنامج
+python snd.py
